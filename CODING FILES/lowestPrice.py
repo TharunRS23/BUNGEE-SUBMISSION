058293e297440cae6dd5f454452b3dd5bc7731e2
THARUN <87370252+TharunRS23@users.noreply.github.com>
2021-08-29 13:34:11 +0530

    Add files via upload

---
+++ CODING FILES/lowestPrice.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import pandas as pd
+
+data = pd.read_csv("C:\\Users\\HP\\Desktop\\filteredCountry.csv", usecols=["SKU","PRICE"])
+df = data.groupby('SKU').min()["PRICE"]
+print(df)
+
+
+
